b3154e49920570feeab8bf31cb9d57154e11e5e3
zima <yeenimal@pm.me>
2024-10-04 08:47:32 -0700

    Remove create-release.sh as it is being included in the built modpack

--- Versions/Fabric/1.21.1/create-release.sh
+++
@@ -1,8 +0,0 @@
-version=$(grep -m 1 "version = \"" pack.toml | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
-target=$(grep -m 1 "minecraft = \"" pack.toml | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
-echo "Creating $version for $target..."
-#echo "\"$version\""
-
-mkdir ./build
-packwiz mr export -o "./build/$version+$target-zimas-vanillaplus.mrpack"
-packwiz cf export -o "./build/$version+$target-zimas-vanillaplus.zip"
